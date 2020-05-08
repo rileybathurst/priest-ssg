d762de7d1f0da3f2b5498a233b95e2ec14128dc1
Riley <riley@rileybathurst.com>
2020-05-08 14:40:54 -0700

    this works on a single class test-input but needs to go to qSA

--- site/src/pages/order.js
+++ site/src/pages/order.js
@@ -3,68 +3,128 @@ import Layout from '../components/layout'
 import HeaderContact from "../components/headercontact"
 import Seo from "../components/seo"
 
-/* // im pretty sure this is jquery which surley we can skip
-$(".field__input").on("input", function () {
-
-  // this seems kinda interesting to be able to find all
-  // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
-  var $field = $(this).closest(".field");
-
-  // if value?
-  if (this.value) {
-
-    // just adding the field--not-empty seems pretty chill
-    $field.addClass("field--not-empty");
-  } else {
-    $field.removeClass("field--not-empty");
-  }
-}); */
-
-// start rewriting as a test
-
-function Input() {
-  var test_input = document.querySelector('.test-input');
-
-  /* if (test_input !== null) {
-    test_input.classList.add("field--not-empty");
-  } */
-
-  // innerhtml to make things?
-  // wait I have stateful react I can probably use that???
-
-  return <input type="text" name="name" id="name" required className="test-input" />;
-}
-
-
-class NameForm extends React.Component {
+// Handling Multiple Inputs https://reactjs.org/docs/forms.html#handling-multiple-inputs
+class Reservation extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-    this.handleChange = this.handleChange.bind(this);
+    // were not using defaults with how it shows the label before a value has been entered
+    this.state = {
+      // customer: 'Priest Sheetmetal',
+      // email: 'info@priestsheetmetal.co.nz',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {    this.setState({value: event.target.value});  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value; // this has to have some extra if its a checkbox or other non text
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+
+    if (this.state.customer !== '') {
+      // console.log('things');
+
+      // theres a forEach way I have done this before but closest seems like a nice solution
+      // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+      // var nearby = test_input.closest('div'); // this doesnt like working on a querySelectorAll
+      // console.log(nearby);
+
+      
+
+      var test_input = document.querySelector('.test-input');
+      test_input.classList.add("field--not-empty");
+
+
+
+/*       nearby.classList.add("field--not-empty");
+    } else { // theres a problem with this if something is entered then removed the classlist doesnt update first charachter is rentered 
+      console.log('elsed');
+      // console.log('check = ' + this.state.customer);
+      test_input.classList.remove("field--not-empty");
+      */
+    } 
+
+    // in the wierdest way it doesnt update when the value is removed
+
+// || double pipe for or
+/*     if (this.state.customer === '') {
+      console.log('nothings');
+      console.log('check = ' + this.state.customer);
+      test_input.classList.remove("field--not-empty");
+    } */
+  }
+
   handleSubmit(event) {
-    // alert('A name was submitted: ' + this.state.value);
-    window.open('mailto:test@example.com?subject=subject&body=body');
 
-
-
+    // nothing gets submitted it just opens a new window
+    window.open('mailto:' + this.state.email + '?subject=Priest Sheetmetal Quote Enquiry&body=Priest Sheetmetal quote enquiry%0D%0Abreak ' + this.state.customer);
 
     event.preventDefault();
-
-
-
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />        </label>
-        <input type="submit" value="Submit" />
+
+        <div className="order-form-section test-input">
+          <label> {/* react notes show wrapping the label but im using a grid to overlay what I want */}
+            Name:
+            </label>
+            <input
+              name="customer"
+              type="text"
+              value={this.state.customer}
+              onChange={this.handleInputChange}
+            />
+
+        </div>
+
+        <div className="order-form-section test-input">
+          <label>
+            Number of guests:
+            </label>
+            <input
+              name="email"
+              type="email"
+              value={this.state.email}
+              onChange={this.handleInputChange} />
+
+        </div>
+
+{/* 
+        <div className="order-form-section">
+          <label for="metal">Metal type:
+            <select name="metal" id="metal-select">
+              <option value="">--Please choose an option--</option>
+              <option value="Brass">Brass</option>
+              <option value="Steel">Steel</option>
+            </select>
+          </label>
+        </div>
+        
+        <div className="order-form-section">
+          <label for="thickness">Thickness:</label>
+          <input type="range" id="thickness" name="thickness" min="10" max="100" />
+        </div>
+
+        <div className="order-form-section">
+          <label for="metal">Finish:
+            <select name="finish" id="finish-select">
+              <option value="">--Please choose an option--</option>
+              <option value="polished">Polished</option>
+              <option value="brushed">Brushed</option>
+            </select>
+          </label>
+        </div> */}
+
+        <div className="order-form-submit">
+          <input type="submit" value="Submit" />
+        </div>
       </form>
     );
   }
@@ -82,50 +142,7 @@ const ContactPage = () => (
 
       <p>Fill in this form to get inline to order.</p>
 
-      <form action="" method="get" class="order-form">
-
-
-        {/* <Input /> */}
-
-        <NameForm />
-
-       {/*  <div class="order-form-section">
-          <label for="name">Enter your name: </label>
-          <input type="text" name="name" id="name" required className="test-input" />
-        </div> */}
-
-       {/*  <div class="order-form-section">
-          <label for="email">Enter your email: </label>
-          <input type="email" name="email" id="email" required />
-        </div> */}
-
-        <div class="order-form-section">
-          <label for="metal">Metal type:</label>
-          <select name="metal" id="metal-select">
-            <option value="">--Please choose an option--</option>
-            <option value="Brass">Brass</option>
-            <option value="Steel">Steel</option>
-          </select>
-        </div>
-        
-        <div class="order-form-section">
-          <label for="thickness">Thickness:</label>
-          <input type="range" id="thickness" name="thickness" min="10" max="100" />
-        </div>
-
-        <div class="order-form-section">
-          <label for="metal">Finish:</label>
-          <select name="finish" id="finish-select">
-            <option value="">--Please choose an option--</option>
-            <option value="polished">Polished</option>
-            <option value="brushed">Brushed</option>
-          </select>
-        </div>
-
-        <div class="order-form-section">
-          <input type="submit" value="Order" />
-        </div>
-      </form>
+      <Reservation />
 
     </main>
 
